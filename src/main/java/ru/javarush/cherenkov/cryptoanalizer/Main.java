56ac3903caebd2f4d1eea7f8019e63de9998ee08
Slavka Cherenkov <slavka.cherenkov@gmail.com>
2024-08-27 12:34:10 +0300

    init commit

---
+++ src/main/java/ru/javarush/cherenkov/cryptoanalizer/Main.java
@@ -0,0 +1,7 @@
+package ru.javarush.cherenkov.cryptoanalizer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/ru/javarush/cherenkov/cryptoanalizer/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.cherenkov.cryptoanalizer;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello maven");
+    }
+}
